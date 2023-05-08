1e36a26fc0e4cb7c8a0ba0db9370fc45b8bc63f1
alkan <alkan@DESKTOP-HJ2SIOP>
2023-05-08 22:05:30 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,39 @@
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+
+//SOLID 
+//Open Closed Principle
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        ProductTest();
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        foreach (var category in categoryManager.GetAll())
+        {
+            Console.WriteLine(category.CategoryName);
+        }
+    }
+
+    private static void ProductTest()
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+
+        var result = productManager.GetProductDetails();
+        if (result.Success == true)
+        {
+            foreach (var product in result.Data)
+            {
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+            }
+        }
+        else
+        {
+            Console.WriteLine(result.Message);
+        }
+    }
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,20 @@
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataAccess.Abstract
+{
+    //Ürüne ait özel operasyonlar yazılacak. 
+    public interface IProductDal : IEntityRepository<Product>
+    {
+        List<ProductDetailDto> GetProductDetails();
+        
+    }
+}
+// Code Refactoring
